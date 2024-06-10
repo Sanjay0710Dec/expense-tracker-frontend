c62497098c3b473a5815d5c351d845a291d86486
Sanjay0710Dec <sanjaymike0714@gmail.com>
2024-06-10 22:14:58 +0530

    modified height of the page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ function ErrorPage() {
     const error = useRouteError();
     if(isRouteErrorResponse(error)){
       return (
-        <div>
+        <div className="min-h-screen  bg-slate-500/40 flex  justify-center items-center px-3 sm:px-0 ">
           <h1>Oops!</h1>
           <h2>{error.status}</h2>
           <p>{error.statusText}</p>
@@ -13,11 +13,11 @@ function ErrorPage() {
     }
         return (
            
-          <div  className="h-screen bg-slate-500/40 flex  justify-center items-center px-3 sm:px-0 ">
-            <div id="semi-container" className="shadow-md py-1 px-2 flex flex-col justify-center items-center rounded-lg">
+          <div  className="min-h-screen bg-slate-500/40 flex  justify-center items-center px-3 sm:px-0 ">
+            <div id="semi-container" className="shadow-lg shadow-white py-1 px-2 flex flex-col justify-center items-center rounded-lg">
             <h1 className="text-3xl">Oops!</h1>
             <h2 className="text-2xl">Page Not Found</h2>
-            <h2 className="text-2xl">{"I  will give a chance to develop this page , so that you won't encounter this error in future(just joking !!!)"}</h2>
+            <h2 className="text-2xl text-center">{"I  will give a chance to develop this page , so that you won't encounter this error in future(just joking !!!)"}</h2>
             </div>
           </div>
         );
